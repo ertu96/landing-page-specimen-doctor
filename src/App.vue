272a6e4dbb3fe8a31c4ef97cb2667ce78a857dfd
ertu96 <48797125+ertu96@users.noreply.github.com>
2023-01-15 16:19:44 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Container from './components/layout/Container.vue'
+import Footer from './components/layout/Footer.vue'
+import Header from './components/layout/Header.vue'
+</script>
+
+<template>
+    <Header />
+    <Container> <RouterView /> </Container>
+    <Footer />
+</template>
