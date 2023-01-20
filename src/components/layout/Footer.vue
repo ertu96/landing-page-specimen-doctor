e69454fac8d889e8b92ccb657a3301c59c5cb9db
ertu96 <48797125+ertu96@users.noreply.github.com>
2023-01-20 14:13:00 +0100

    Footer Styling

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,29 +1,44 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+            links: [
+                {
+                    name: 'Impressum',
+                    href: '#',
+                },
+                {
+                    name: 'Datenschutzerklärung',
+                    href: '#',
+                },
+                {
+                    name: 'Cookies',
+                    href: '#',
+                },
+            ],
+        }
+    },
+})
+</script>
+
 <template>
-    <footer
-        class="w-full border-t border-gray-200 bg-white p-4 shadow dark:border-gray-600 dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6"
-    >
-        <span class="text-sm text-gray-500 dark:text-gray-400 sm:text-center"
-            >© 2023
-            <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a
-            >. All Rights Reserved.
-        </span>
-        <ul
-            class="mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
-        >
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6"
-                    >Privacy Policy</a
+    <footer class="border-t bg-primary-900 text-neutral-200">
+        <div class="mx-auto flex max-w-screen-xl justify-between py-4">
+            <span class="text-sm sm:text-center"
+                >© 2023
+                <a href="https://flowbite.com/" class="hover:underline"
+                    >Praxis Mustermann</a
                 >
-            </li>
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
-            </li>
-            <li>
-                <a href="#" class="hover:underline">Contact</a>
-            </li>
-        </ul>
+            </span>
+            <ul class="mt-3 flex flex-wrap items-center text-sm sm:mt-0">
+                <li v-for="link in links" :key="link.name">
+                    <a :href="link.href" class="mr-4 hover:underline md:mr-6">{{
+                        link.name
+                    }}</a>
+                </li>
+            </ul>
+        </div>
     </footer>
 </template>
